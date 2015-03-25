47f22e2d728ee16cdb7f8c308636d512e7248d14
Matias Solis de la Torre <matias.delatorre89@gmail.com>
2015-03-25 19:21:46 -0300

    Se corrige el decimal del precio del rodado

--- src/BigD/PersonasBundle/Entity/Rodado.php
+++ src/BigD/PersonasBundle/Entity/Rodado.php
@@ -46,7 +46,7 @@ class Rodado {
     /**
      * @var string
      *
-     * @ORM\Column(name="precio", type="decimal", nullable=true)
+     * @ORM\Column(name="precio", type="decimal", scale=2, nullable=true)
      */
     private $precio;
 
